6a47109112e0c85739fd0e86b51efd26f64073ed
Caroline Pisano <pisanocaroline@gmail.com>
2018-05-11 14:10:38 -0400

    Just flinging some stuff at github

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -92,15 +92,15 @@ app.events = function () {
     const selectedDevice = $(this).val();
 
     console.log(selectedDevice);
-
-    if (selectedDevice === 'mobile') {
-      console.log('I chose the mobile device.');
-      $('.chosenDevice').css('background-image', 'url(images/iphone-mockup.png)');
-    } else if (selectedDevice === 'tablet') {
-      $('.chosenDevice').css('background-image', 'url(images/ipad-mockup.png)');
-    } else if (selectedDevice === 'desktop') {
-      $('.chosenDevice').css('background-image', 'url(images/macbook-mockup.png)');
+    if (selectedDevice === "mobile") {
+      console.log("I chose the mobile device.");
+      $(".chosenDevice").css("background-image", "url(images/iphone-mockup.png)");
+    } else if (selectedDevice === "tablet") {
+      $(".chosenDevice").css("background-image", "url(images/ipad-mockup.png)");
+    } else if (selectedDevice === "desktop") {
+      $(".chosenDevice").css("background-image", "url(images/macbook-mockup.png)");
     }
+
   })
 
   // caroline's events
